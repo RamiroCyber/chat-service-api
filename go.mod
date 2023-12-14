805021e331b6cc723f5dc8286155fba3c581dc2d
RamiroCyber <ramiro.dr@gmail.com>
2023-12-13 23:27:14 -0300

    adicionando lib para contagem de tokens

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module CHAT_SERVICE_API
 
 go 1.21
 
-require github.com/google/uuid v1.5.0 // indirect
+require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+)
--- internal/domain/entity/message.go
+++ internal/domain/entity/message.go
@@ -4,6 +4,7 @@ import (
 	"CHAT_SERVICE_API/util"
 	"errors"
 	"github.com/google/uuid"
+	"github.com/pkoukk/tiktoken-go"
 	"time"
 )
 
@@ -17,11 +18,19 @@ type Message struct {
 }
 
 func NewMessage(role int, content string, model *Model) (*Message, error) {
+	tkm, err := tiktoken.EncodingForModel(model.GetName())
+	if err != nil {
+		return nil, err
+	}
+
+	tokens := tkm.Encode(content, nil, nil)
+
 	msg := &Message{
 		ID:        uuid.New().String(),
 		Role:      role,
 		Content:   content,
 		Model:     model,
+		Tokens:    len(tokens),
 		CreatedAt: time.Now(),
 	}
 	if err := msg.Validate(); err != nil {
