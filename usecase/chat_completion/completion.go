c5f3d24672e445b26bbaf54dbe62bf9e09eb95b2
RamiroCyber <ramiro.dr@gmail.com>
2024-01-04 20:46:07 -0300

    adicionando  construtor

--- usecase/chat_completion/completion.go
+++ usecase/chat_completion/completion.go
@@ -9,3 +9,10 @@ type ChatCompletionUseCase struct {
 	ChatGateway  gateway.ChatGateway
 	OpenAiClient *openai.Client
 }
+
+func NewChatCompletionUseCase(chatGateway gateway.ChatGateway, openAiClient *openai.Client) *ChatCompletionUseCase {
+	return &ChatCompletionUseCase{
+		ChatGateway:  chatGateway,
+		OpenAiClient: openAiClient,
+	}
+}
