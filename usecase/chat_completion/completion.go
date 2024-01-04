73f6540de0a9c682ef9aca79f21cb70720b09d48
RamiroCyber <ramiro.dr@gmail.com>
2024-01-03 21:45:01 -0300

    adicionando lib para integrar com o cliente da openai

--- usecase/chat_completion/completion.go
+++ usecase/chat_completion/completion.go
@@ -1,1 +1,11 @@
 package chat_completion
+
+import (
+	"CHAT_SERVICE_API/internal/domain/gateway"
+	"github.com/sashabaranov/go-openai"
+)
+
+type ChatCompletionUseCase struct {
+	ChatGateway  gateway.ChatGateway
+	OpenAiClient *openai.Client
+}
